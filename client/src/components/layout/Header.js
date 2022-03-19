77df1faf719e22282566268516fa334106a27bf4
Ayush Gupta <ayushgupta@192.168.1.2>
2022-03-19 22:33:27 +0530

    completed the development of Header section and also added rect-icons library

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,40 +1,79 @@
-// import {  } from 'react-router-dom';
-import React from 'react';
-import logo from '../../../public/images/logo_2x.png';
+// react
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
+// components
+import { ImCart } from 'react-icons/im';
+
+// images
+import desktopLogo from '../../../public/images/logo_2x.png';
+import mobileLogo from '../../../public/images/logo.png';
+
+/**
+ * @returns JSX for rendering Header Section
+ */
 const Header = () => {
+  // component states
+  const [showMobileMenuOverlay, setShowMobileMenuOverlay] = useState(false);
+
   return (
-    <header className="header">
-      <section className="left-section">
-        <nav className="nav-links">
+    <header className={showMobileMenuOverlay ? 'header open' : 'header'}>
+      {/* mobile hamburger menu items */}
+      {showMobileMenuOverlay && (
+        <div className="overlay hide-in-desktop hide-in-tablet">
+          <div className="header__menu">
+            <Link to="/">Home</Link>
+            <Link to="/">Products</Link>
+            <Link to="/">Login</Link>
+            <Link to="/">SignUp</Link>
+          </div>
+        </div>
+      )}
+
+      <section className="header__left-section">
+        <nav className="header__left-section__nav-links">
           <ul>
             <li>
-              <a to="/">
-                <img src={logo} alt="logo" />
-              </a>
+              <Link to="/" className="header__left-section__nav-links__logo">
+                <img className="hide-in-mobile" src={mobileLogo} alt="logo" />
+                <img className="hide-in-tablet" src={desktopLogo} alt="logo" />
+              </Link>
             </li>
-            <li>
-              <a to="/">Home</a>
+            <li className="hide-in-mobile">
+              <Link to="/">Home</Link>
             </li>
-            <li>
-              <a to="/">Products</a>
+            <li className="hide-in-mobile">
+              <Link to="/">Products</Link>
             </li>
           </ul>
         </nav>
       </section>
 
-      <section className="right-section">
-        <nav className="nav-links">
+      <section className="header__right-section">
+        <nav className="header__right-section__nav-links hide-in-mobile">
           <ul>
             <li>
-              <a to="/">Login</a>
+              <Link to="/">Login</Link>
             </li>
             <li>
-              <a to="/">SignUp</a>
+              <Link to="/">SignUp</Link>
             </li>
           </ul>
         </nav>
-        <div className="cart">0 items</div>
+
+        <div className="header__right-section__cart">
+          <ImCart className="header__right-section__cart__icon" />
+          <span>0 items</span>
+        </div>
+
+        <button
+          id="hamburger"
+          className="header__hamburger hide-in-tablet"
+          onClick={() => setShowMobileMenuOverlay((prevState) => !prevState)}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
       </section>
     </header>
   );
